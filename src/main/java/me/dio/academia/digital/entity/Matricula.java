8a2f3caa99eb6240794f8bafc1b0bbfd5c86bbc9
Paulo Vargas <paulotomegomesdevargas@hotmail.com>
2022-08-20 14:04:11 -0300

    Até crud parte 1

--- src/main/java/me/dio/academia/digital/entity/Matricula.java
+++ src/main/java/me/dio/academia/digital/entity/Matricula.java
@@ -1,11 +1,28 @@
 package me.dio.academia.digital.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_matriculas")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Matricula {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
 
   private Long id;
 
+  @OneToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "aluno_id")
   private Aluno aluno;
 
   private LocalDateTime dataDaMatricula = LocalDateTime.now();
